f30ce9ac3c98a751f5fe2417722845ceb019d756
Yejin Jeon <97823928+yaezzin@users.noreply.github.com>
2023-12-12 03:59:23 +0900

    Title: 함수를 이용한 부분 문자열의 위치 구하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231212/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.py
@@ -0,0 +1,20 @@
+a = list(input())
+b = list(input())
+
+
+def func():
+    tmp = -1
+    
+    for i in range(len(a) - 1):
+
+        for j in range(len(b)):
+
+            if a[i+j] == b[j]:
+                tmp = i + j
+        
+        if tmp != -1:
+            tmp -= len(b) - 1
+            
+    return tmp
+
+print(func())
