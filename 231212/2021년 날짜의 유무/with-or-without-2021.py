058bdfe3d59bc21234db933ddc0afafdf357996d
Yejin Jeon <97823928+yaezzin@users.noreply.github.com>
2023-12-12 02:56:15 +0900

    Title: 2021년 날짜의 유무, Time: 204ms, Memory: 24MB, Status: Passed - CodeTree

--- 231212/2021년 날짜의 유무/with-or-without-2021.py
+++ 231212/2021년 날짜의 유무/with-or-without-2021.py
@@ -1,25 +1,23 @@
-def is_correct_day(m, d):
-    if m > 0 and m < 13:
-
-        if m == 1 or m == 3 or m == 5 or m == 7 or m == 8 or m == 10 or m == 12:
-            if d < 32:
-                return True
-        
-        elif m == 2:
-            if d < 29:
-                return True
-        
-        else:
-            if d < 31:
-                return True
-        
-        return False
+def is_correct(m, d):
+    if m < 13 and d <= last_day(m, d):
+        return True
     
     return False
 
+def last_day(m, d):
+    if m == 1 or m == 3 or m == 5 or m == 7 or m == 8 or m == 10 or m == 12:
+        return 31
+    
+    if m == 2:
+        return 28
+    
+    else:
+        return 30
+
+
 
 m, d = map(int, input().split())
-if is_correct_day(m, d):
+if is_correct(m, d):
     print('Yes')
 else:
     print('No')
