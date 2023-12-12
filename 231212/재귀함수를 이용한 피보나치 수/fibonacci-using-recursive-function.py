acf7eb44a802023fdef858215a3a0f2e889c6f5a
Yejin Jeon <97823928+yaezzin@users.noreply.github.com>
2023-12-12 19:16:30 +0900

    Title: 재귀함수를 이용한 피보나치 수, Time: 177ms, Memory: 26MB, Status: Passed - CodeTree

---
+++ 231212/재귀함수를 이용한 피보나치 수/fibonacci-using-recursive-function.py
@@ -0,0 +1,12 @@
+def fibonachi(n):
+    if n == 0:
+        return 0
+    
+    elif n == 1 or n == 2:
+        return 1
+
+    return fibonachi(n-1) + fibonachi(n-2)
+
+
+n = int(input())
+print(fibonachi(n))
