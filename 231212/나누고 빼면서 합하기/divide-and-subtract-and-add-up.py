a477eaafe8cf82d0142db2166b8d30dff5436f29
Yejin Jeon <97823928+yaezzin@users.noreply.github.com>
2023-12-12 17:25:26 +0900

    Title: 나누고 빼면서 합하기, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 231212/나누고 빼면서 합하기/divide-and-subtract-and-add-up.py
+++ 231212/나누고 빼면서 합하기/divide-and-subtract-and-add-up.py
@@ -1,16 +1,21 @@
 n, m = map(int, input().split())
 lst = list(map(int, input().split()))
 
+def get_answer():
+    # m을 가져와서 사용하기 위해서 global 붙여주기
+    global m
 
-s = 0
-while m >= 1:
-    
-    s += lst[m-1]
+    s = 0
+    while m >= 1:
+        
+        s += lst[m - 1]
 
-    if m % 2 == 0:
-        m //= 2
-    
-    else:
-        m -= 1
+        if m % 2 == 0:
+            m //= 2
+        
+        else:
+            m -= 1
 
-print(s)
+    return s
+
+print(get_answer())
