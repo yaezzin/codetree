5b05480a81c992487fc79ecadf7a3e051e2ae0a8
Yejin Jeon <97823928+yaezzin@users.noreply.github.com>
2023-12-12 17:37:25 +0900

    Title: 특정 구간의 합, Time: 140ms, Memory: 25MB, Status: Passed - CodeTree

--- 231212/특정 구간의 합/sum-of-specific-intervals.py
+++ 231212/특정 구간의 합/sum-of-specific-intervals.py
@@ -1,11 +1,15 @@
 n, m = map(int, input().split())
 lst_a = list(map(int, input().split()))
 
-for _ in range(m):
-    a1, a2 = map(int, input().split())
-
+def get_answer(a1, a2):
     s = 0
-    for i in range(a1-1, a2):
+
+    for i in range(a1 - 1, a2):
         s += lst_a[i]
+        
+    return s 
     
-    print(s)
+        
+for _ in range(m):
+    a1, a2 = map(int, input().split()) 
+    print(get_answer(a1, a2))
