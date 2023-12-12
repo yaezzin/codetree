77392db9bd18e557261a67a551917c712d5128de
Yejin Jeon <97823928+yaezzin@users.noreply.github.com>
2023-12-13 02:09:25 +0900

    Title: 코드네임, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

--- 231213/코드네임/code-name.py
+++ 231213/코드네임/code-name.py
@@ -5,8 +5,10 @@ user = [tuple(input().split()) for _ in range(5)]
 m = sys.maxsize
 answer = ()
 for u in user:
-    if int(u[1]) < m:
-        m = int(u[1])
+    name, score = u
+
+    if int(score) < m:
+        m = int(score)
         answer = u
 
 print(*answer)
