96c4a618c45e7ec1dd8a7783d343207e0a993f16
Yejin Jeon <97823928+yaezzin@users.noreply.github.com>
2023-12-13 01:47:33 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 231213/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
+++ 231213/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
@@ -1,23 +1,19 @@
 def find_gcd(x, y):
     while y:
         x, y = y, x % y
-    
     return x
 
-def find_lcm(x, y):
+def find_lcm_recursive(x, y):
     gcd = find_gcd(x, y)
-    
     return x * y // gcd
 
 def find_lcm_recursive_list(numbers):
     if len(numbers) == 2:
-        return find_lcm(numbers[0], numbers[1])
+        return find_lcm_recursive(numbers[0], numbers[1])
     else:
-        return find_lcm_recursive_list([find_lcm(numbers[0], numbers[1])] + numbers[2:])
-
+        return find_lcm_recursive_list([find_lcm_recursive(numbers[0], numbers[1])] + numbers[2:])
 
 n = int(input())
 numbers = list(map(int, input().split()))
-
 lcm_result = find_lcm_recursive_list(numbers)
 print(lcm_result)
