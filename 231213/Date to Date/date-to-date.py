9cfb59d833292c4ffb54a3a714e832b57341650b
Yejin Jeon <97823928+yaezzin@users.noreply.github.com>
2023-12-13 17:13:01 +0900

    Title: Date to Date, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231213/Date to Date/date-to-date.py
+++ 231213/Date to Date/date-to-date.py
@@ -3,7 +3,7 @@ m1, d1, m2, d2 = map(int, input().split())
 num_of_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 # 날짜 1에 대해 총 며칠인지 구하기
-total1 = 1
+total1 = 0
 for i in range(m1):
     total1 += num_of_days[i]
 
