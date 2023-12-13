f8139e38dbe88e4669560bb7bdf18206d75bebb3
Yejin Jeon <97823928+yaezzin@users.noreply.github.com>
2023-12-13 19:49:49 +0900

    Title: 최대로 겹치는 구간, Time: 145ms, Memory: 25MB, Status: Passed - CodeTree

--- 231213/최대로 겹치는 구간/maximum-overlapped-segments.py
+++ 231213/최대로 겹치는 구간/maximum-overlapped-segments.py
@@ -1,11 +1,14 @@
 n = int(input())
 
-lst = [0 for _ in range(100)]
+lst = [0 for _ in range(201)]
 
 for _ in range(n):
     s, e = map(int, input().split())
+    
+    s += 100
+    e += 100
 
     for i in range(s, e):
         lst[i] += 1
-    
-print(lst.count(max(lst)) - 1)
+
+print(max(lst))
