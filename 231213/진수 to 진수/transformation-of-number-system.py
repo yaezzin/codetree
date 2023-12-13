69c91ff3773f7d2cfbf10844e5cdf12c47318058
Yejin Jeon <97823928+yaezzin@users.noreply.github.com>
2023-12-13 19:26:56 +0900

    Title: 진수 to 진수, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

--- 231213/진수 to 진수/transformation-of-number-system.py
+++ 231213/진수 to 진수/transformation-of-number-system.py
@@ -1,25 +1,25 @@
-a, b = map(int, input().split())
-n = int(input())
-
-def decimal(k):
+def decimal(k, a):
     lst = [int(digit) for digit in str(k)]
 
     m = 0
     for i in range(len(lst)):
-        m = m * 8 + lst[i]
+        m = m * a + lst[i]
     
     return m
 
-def binary(k):
+def binary(k, b):
     binary_lst = []
     while k != 0:
-        binary_lst.append(k % 2)
-        k //= 2
+        binary_lst.append(k % b)
+        k //= b
 
     return binary_lst[::-1]
 
 # 8진수로 표현된걸 10진수로 변환
-tmp = decimal(n)
-result = binary(tmp) 
+a, b = map(int, input().split())
+n = int(input())
+
+tmp = decimal(n, a)
+result = binary(tmp, b) 
 
 print(''.join(map(str, result)))
