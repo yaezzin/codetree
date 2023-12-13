771a8ba05bff8b00ef7be857e81a683adf47679b
Yejin Jeon <97823928+yaezzin@users.noreply.github.com>
2023-12-13 17:42:24 +0900

    Title: 그 요일은, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231213/그 요일은/the-day-of-the-day.py
+++ 231213/그 요일은/the-day-of-the-day.py
@@ -1,5 +1,3 @@
-m1, d1, m2, d2 = map(int, input().split())
-
 def func(m, d):
     days = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
@@ -11,5 +9,15 @@ def func(m, d):
 
     return total
 
+m1, d1, m2, d2 = map(int, input().split())
+input_dow = input()
+
+dow = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+if d1 != d2:
+    i = 0
+    while dow[i] != input_dow:
+        d1 += 1
+        i += 1 
+
 diff = func(m2, d2) - func(m1, d1)
-print(diff % 7)
+print(diff // 7 + 1)
