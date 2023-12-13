ba134b55198e7cddfdc06749db6b1db77cb31609
Yejin Jeon <97823928+yaezzin@users.noreply.github.com>
2023-12-13 18:10:32 +0900

    Title: 그 요일은, Time: 134ms, Memory: 73MB, Status: Passed - CodeTree

--- 231213/그 요일은/the-day-of-the-day.py
+++ 231213/그 요일은/the-day-of-the-day.py
@@ -1,9 +1,7 @@
-import math
+days = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+dow = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 
-# 며칠 차이가 나는지 확인하기
 def func(m, d):
-    days = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-
     total = 0
     for i in range(m):
         total += days[i]
@@ -12,28 +10,15 @@ def func(m, d):
 
     return total
 
-# 월요일과 x요일이 얼마나 차이나는지 확인
 def func2(input_dow):
-    dow = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    for idx, value in enumerate(dow):
+        if value == input_dow:
+            return idx
 
-    cnt = 0
-    i = 0    
-    
-    while dow[i] != input_dow:
-        cnt += 1
-        i += 1
-
-    return cnt 
-    
 
 m1, d1, m2, d2 = map(int, input().split())
 input_dow = input()
 
+d1 += func2(input_dow)
 diff = func(m2, d2) - func(m1, d1)
-diff_dow = func2(input_dow)
-
-#print(diff)
-if diff % 7 > diff_dow:
-    print(diff // 7 + 1)
-else:
-    print(diff // 7)
+print(diff // 7 + 1)
