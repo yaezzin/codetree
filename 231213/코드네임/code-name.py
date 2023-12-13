c256b736c60e65fe7badf398a9c801c80f6f510c
Yejin Jeon <97823928+yaezzin@users.noreply.github.com>
2023-12-13 16:33:22 +0900

    Title: 코드네임, Time: 123ms, Memory: 24MB, Status: Passed - CodeTree

--- 231213/코드네임/code-name.py
+++ 231213/코드네임/code-name.py
@@ -1,14 +1,18 @@
 import sys
 
-user = [tuple(input().split()) for _ in range(5)]
+users = [ ]
+for _ in range(5):
+    name, score = tuple(input().split())
+    users.append((name, int(score)))
 
 m = sys.maxsize
-answer = ()
-for u in user:
-    name, score = u
 
-    if int(score) < m:
-        m = int(score)
-        answer = u
+answer = ()
+for user in users:
+    name, score = user
+
+    if score < m:
+        m = score
+        answer = user
 
 print(*answer)
