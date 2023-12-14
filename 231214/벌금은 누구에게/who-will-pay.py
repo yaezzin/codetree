456d2b0e58dcc2473fae6f7a1c7977d9a957150a
Yejin Jeon <97823928+yaezzin@users.noreply.github.com>
2023-12-14 23:44:19 +0900

    Title: 벌금은 누구에게, Time: 163ms, Memory: 25MB, Status: Passed - CodeTree

--- 231214/벌금은 누구에게/who-will-pay.py
+++ 231214/벌금은 누구에게/who-will-pay.py
@@ -3,14 +3,15 @@ n, m, k = map(int, input().split())
 # k번 이상 벌칙을 받으면 벌금!
 lst = [0] * (n + 1)
 
-for _ in range(m):
+answer = []
+for i in range(m):
     stuent_num = int(input())
     lst[stuent_num] += 1
 
-answer = -1
-for i in range(len(lst)):
-    if lst[i] >= k:
-        answer = i
-        break
+    if max(lst) >= k:
+        answer.append(stuent_num)
 
-print(answer)
+if len(answer) == 0:
+    print(-1)
+else:
+    print(answer[0])
