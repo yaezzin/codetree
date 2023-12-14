757f680f6c57c25f06b6a3d78c36b1fbb8631de8
Yejin Jeon <97823928+yaezzin@users.noreply.github.com>
2023-12-14 23:31:40 +0900

    Title: 만나는 그 순간, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231214/만나는 그 순간/the-moment-we-meet.py
@@ -0,0 +1,29 @@
+MAX_R = 30
+
+n, m = map(int, input().split())
+pos_a, pos_b = [0] * (MAX_R + 1), [0] * (MAX_R + 1)
+
+
+time_a = 1
+for _ in range(n):
+    d, t = tuple(input().split())
+    
+    for _ in range(int(t)):
+        pos_a[time_a] = pos_a[time_a - 1] + (1 if d == 'R' else -1)
+        time_a += 1
+
+time_b = 1
+for _ in range(m):
+    d, t = tuple(input().split())
+
+    for i in range(int(t)):
+        pos_b[time_b] = pos_b[time_b -1] + (1 if d == 'R' else - 1)
+        time_b += 1
+
+ans = -1
+for i in range(1, time_a):
+    if pos_a[i] == pos_b[i]:
+        answer = i
+        break
+
+print(answer)
