ad6911e859e848689fa5532beefd17175840fd62
Yejin Jeon <97823928+yaezzin@users.noreply.github.com>
2023-12-14 22:05:29 +0900

    Title: 연속되는 수 2, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231214/연속되는 수 2/continuous-number2.py
@@ -0,0 +1,21 @@
+n = int(input())
+
+lst = []
+for _ in range(n):
+    lst.append(int(input()))
+
+
+change_point = []
+for i in range(len(lst)):
+    if i == 0 or lst[i] != lst[i-1]:
+        change_point.append(i)
+
+change_point = change_point[::-1]
+
+m = 1
+for i in range(1, len(change_point) ):
+    tmp = change_point[i-1] - change_point[i]
+    m = max(tmp, m)
+
+
+print(m)
