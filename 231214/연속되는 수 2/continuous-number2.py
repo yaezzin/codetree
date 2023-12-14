a43a1359e0dc5febefd11492dd2cdabb04e43bed
Yejin Jeon <97823928+yaezzin@users.noreply.github.com>
2023-12-14 22:09:01 +0900

    Title: 연속되는 수 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 231214/연속되는 수 2/continuous-number2.py
+++ 231214/연속되는 수 2/continuous-number2.py
@@ -4,19 +4,21 @@ lst = []
 for _ in range(n):
     lst.append(int(input()))
 
-
+# 변하는 구간의 idx 넣기 
 change_point = []
 for i in range(len(lst)):
     if i == 0 or lst[i] != lst[i-1]:
         change_point.append(i)
 
+# 거꾸로 뒤집어서 인덱스 간의 거리 계산
 change_point = change_point[::-1]
 
-m = 0
+max_value = 0
 for i in range(1, len(change_point)):
     tmp = change_point[i-1] - change_point[i]
-    m = max(tmp, m)
+    max_value = max(tmp, max_value)
 
+# 만약 max_value가 0이라면 모든 리스트가 동일한 숫자로 이루어졌다는 것
 if m == 0:
     print(len(lst))
 else:
