f05575acee0949e9cc170900958c2f90acfd8a13
Yejin Jeon <97823928+yaezzin@users.noreply.github.com>
2023-12-18 02:44:25 +0900

    Title: 이상한 진수 2, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

--- 231218/이상한 진수 2/awkward-digits-2.py
+++ 231218/이상한 진수 2/awkward-digits-2.py
@@ -9,15 +9,24 @@ def decimal(lst):
 
 answer = 0
 for i in range(len(s)):
-    if s[i] == '0':
-        s[i] = '1'
-
+    if s[i] == '1':
+        s[i] = '0'
         # 2진법을 => 10진법으로 바꾸기
         result = decimal(s)
 
         # 최대값 구하기
         answer = max(result, answer)
-        
+    
+        s[i] = '1'
+    else:
+        s[i] = '1'
+        # 2진법을 => 10진법으로 바꾸기
+        result = decimal(s)
+
+        # 최대값 구하기
+        answer = max(result, answer)
+    
         s[i] = '0'
+
 
 print(answer)
