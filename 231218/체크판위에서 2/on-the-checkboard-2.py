133afc880b29b255f661d96ed17d7ad41ebb0135
Yejin Jeon <97823928+yaezzin@users.noreply.github.com>
2023-12-18 02:24:32 +0900

    Title: 체크판위에서 2, Time: 121ms, Memory: 25MB, Status: Passed - CodeTree

--- 231218/체크판위에서 2/on-the-checkboard-2.py
+++ 231218/체크판위에서 2/on-the-checkboard-2.py
@@ -3,11 +3,11 @@ lst = [ list(input().split(' ')) for _ in range(n) ]
 
 answer = 0
 for i in range(1, n-1):   
-    for j in range(1, m-1):     
+    for j in range(1, m-1): 
         for k in range(i+1, n): 
             for l in range(j+1, m):
-                if lst[0][0] != lst[i][j] and lst[i][j] != lst[k][l] and lst[k][l] != lst[n-1][m-1]:
-                    answer += 1
+                if lst[0][0] != lst[i][j] and lst[i][j] != lst[k][l] and lst[k][l] != lst[n-1][m-1]:      
+                    if k < n - 1 and l < m - 1:
+                        answer += 1
 
-            
-print(answer // 2)
+print(answer)
