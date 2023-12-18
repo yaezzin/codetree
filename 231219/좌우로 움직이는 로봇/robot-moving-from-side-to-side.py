b17926bcf55d1b48dbaa00cca8baf9b9e8b97426
Yejin Jeon <97823928+yaezzin@users.noreply.github.com>
2023-12-19 05:37:20 +0900

    Title: 좌우로 움직이는 로봇, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231219/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
@@ -0,0 +1,51 @@
+time_a = [0] * 50000
+time_b = [0] * 50000
+
+n, m = map(int, input().split())
+
+cur_a = 0
+for _ in range(n):
+    distance, direction = input().split()
+    distance = int(distance)
+
+    for i in range(distance):
+        time_a[cur_a + 1] = time_a[cur_a] + (1 if direction == 'R' else -1)
+        cur_a += 1
+    
+cur_b = 0
+for _ in range(m):
+    distance, direction = input().split()
+    distance = int(distance)
+
+    for i in range(distance):
+        time_b[cur_b + 1] = time_b[cur_b] + (1 if direction == 'R' else -1)
+        cur_b += 1
+
+
+if cur_a > cur_b:
+    for i in range(1, cur_a - cur_b + 1):
+        time_b[cur_b + i] = time_b[cur_b + i -1]
+else:
+    for i in range(1, cur_b - cur_a + 1): # 0 1 2 
+        time_a[cur_a + i] = time_a[cur_a + i - 1]
+
+cnt = 0
+for i in range(1, 50001):
+    if time_a[i] == 0 and time_b[i] == 0:
+        break
+
+    if time_a[i] == time_b[i]:
+        
+        if time_a[i-1] <= time_a[i] and time_b[i-1] > time_b[i]:
+            cnt += 1
+    
+        elif time_a[i-1] >= time_a[i] and time_b[i-1] < time_b[i]:  
+            cnt += 1
+        
+        elif time_a[i-1] > time_a[i] and time_b[i-1] <= time_b[i]:  
+            cnt += 1
+        
+        elif time_a[i-1] < time_a[i] and time_b[i-1] >= time_b[i]:  
+            cnt += 1
+
+print(cnt)
