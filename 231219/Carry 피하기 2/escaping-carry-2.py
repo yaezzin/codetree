5449ccdf3f65d9da225b95388ba6345825a9c14e
Yejin Jeon <97823928+yaezzin@users.noreply.github.com>
2023-12-19 00:19:39 +0900

    Title: Carry 피하기 2, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231219/Carry 피하기 2/escaping-carry-2.py
@@ -0,0 +1,31 @@
+n = int(input())
+lst = [int(input()) for _ in range(n)]
+
+answer = 0
+for i in range(n):
+    for j in range(i + 1, n):
+        for k in range(j + 1, n):
+            
+            first, second, third = lst[i], lst[j], lst[k]
+            multiplier, sum_value, tmp = 1, 0, 0
+
+            # 세 수 중에 한 수라도 0 이상인 동안
+            while first > 0 or second > 0 or third > 0:
+                # 10으로 나눈 나머지를 더해서 승수를 곱해준다.
+                tmp = ((first % 10) + (second % 10) + (third % 10))
+                sum_value += tmp * multiplier
+
+                # 그 값이 10보다 크면 break
+                if tmp >= 10:
+                    break
+                
+                # 그렇지 않으면 10으로 나누어주고 승수도 10을 높인다.
+                first //= 10
+                second //= 10
+                third //= 10
+                multiplier *= 10
+            
+            # 최대값 찾기
+            answer = max(answer, sum_value)
+
+print(answer)
