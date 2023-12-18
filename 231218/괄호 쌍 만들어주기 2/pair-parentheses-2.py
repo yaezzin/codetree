1a8ef1bdf6b5500c1127b7848385ee279a29edce
Yejin Jeon <97823928+yaezzin@users.noreply.github.com>
2023-12-18 23:55:15 +0900

    Title: 괄호 쌍 만들어주기 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231218/괄호 쌍 만들어주기 2/pair-parentheses-2.py
@@ -0,0 +1,11 @@
+lst = list(input())
+
+cnt = 0
+for i in range(len(lst)):
+    if lst[i] == '(' and lst[i+1] == '(':
+
+        for j in range(i+2, len(lst) - 1):
+            if lst[j] == ')' and lst[j+1] == ')':
+                cnt += 1
+
+print(cnt)
