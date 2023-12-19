16d1617fc2275ac0413a53155b625e68fec82d5b
Yejin Jeon <97823928+yaezzin@users.noreply.github.com>
2023-12-19 16:51:38 +0900

    Title: 문자에 따른 명령 2, Time: 111ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 231219/문자에 따른 명령 2/text-based-commands2.py
@@ -0,0 +1,24 @@
+# 동서남북을 동 남 서 북으로 바꾸기
+
+# L이 주어지면 반시계 방향으로
+# R이 주어지면 시계방향으로 방향 전환
+# F가 주어지면 바라보고 있는 방향으로 한칸 이동 
+
+lst = list(input())
+x, y = 0, 0
+current_location = 3 # 현재 북쪽을 보고 있음
+
+dx = [1, 0, -1, 0]
+dy = [0, -1, 0, 1]
+for d in lst:
+    if d == 'L':
+        flag = (current_location -1 + 4) % 4
+    
+    elif d == 'R':
+        flag = (current_location + 1) % 4
+
+    else:
+        x += dx[flag]
+        y += dy[flag]
+
+print(x, y)
