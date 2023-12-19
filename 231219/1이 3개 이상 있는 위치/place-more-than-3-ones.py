f6c2188ef89b872362500ed544e33e6abf361c4b
Yejin Jeon <97823928+yaezzin@users.noreply.github.com>
2023-12-19 17:24:04 +0900

    Title: 1이 3개 이상 있는 위치, Time: 138ms, Memory: 26MB, Status: Passed - CodeTree

--- 231219/1이 3개 이상 있는 위치/place-more-than-3-ones.py
+++ 231219/1이 3개 이상 있는 위치/place-more-than-3-ones.py
@@ -1,5 +1,5 @@
 n = int(input())
-lst = [ list(map(int, input().split(' '))) for _ in range(n)]
+lst = [ list(map(int, input().split())) for _ in range(n)]
 
 # 각 칸 중 상하좌우로 인접한 칸 중 숫자 1이 적혀 있는 칸의 수가 3개 이상인 곳의 개수
 dx = [0, 1, 0, -1]
@@ -7,10 +7,10 @@ dy = [1, 0, -1, 0]
 
 answer = 0
 for x in range(n):
+
     for y in range(n):
         cnt = 0
 
-        
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
