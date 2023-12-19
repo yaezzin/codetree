4ad3369d7fcb504133e8a10cfb0a6832cc21ce89
Yejin Jeon <97823928+yaezzin@users.noreply.github.com>
2023-12-19 16:08:32 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231219/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -0,0 +1,33 @@
+N, K, P, T = map(int, input().split())
+
+# t초에 x와 y가 악수를 한 정보를 입력받는다,
+time = []
+for i in range(T):
+    t, x, y = map(int, input().split())
+    time.append([t, x, y])
+
+# 시간순으로 오름차순으로 정렬한다.
+time.sort(key=lambda x : x[0])
+
+# 처음 감연자의 정보를 딕셔너리에 넣어준다.   
+dic = {}
+dic[P] = 0
+
+for t in time:
+    _, x, y = t
+    
+    # 만약 x가 감염자 리스트에 있고, x의 악수 횟수가 K번 이하이면
+    if x in dic and dic[x] < K:
+        dic[x] += 1 
+        dic[y] = 0
+        
+    elif y in dic and dic[y] < K:
+        dic[y] += 1 
+        dic[x] = 0
+        
+       
+for i in range(1, N+ 1 ):
+    if i in dic:
+        print(1, end='')
+    else:
+        print(0, end='')
