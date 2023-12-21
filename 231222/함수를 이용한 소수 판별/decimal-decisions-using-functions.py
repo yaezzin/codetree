5e3ba17b87276f8f852fcd2fbe5f7f3a76a8ce0f
Yejin Jeon <97823928+yaezzin@users.noreply.github.com>
2023-12-22 00:09:34 +0900

    Title: 함수를 이용한 소수 판별, Time: 123ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231222/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
@@ -0,0 +1,20 @@
+import math
+
+def is_prime(n):
+    if n == 0 or n == 1:
+        return False
+
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            return False
+    
+    return True
+
+a, b = map(int, input().split())
+
+s = 0
+for i in range(a, b+1):
+    if is_prime(i):
+        s += i
+
+print(s)
