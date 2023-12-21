e38e018a5b98afbc7fd66a6d668dccf26b548305
Yejin Jeon <97823928+yaezzin@users.noreply.github.com>
2023-12-21 23:58:49 +0900

    Title: 특정 위치의 숫자 2, Time: 121ms, Memory: 24MB, Status: Passed - CodeTree

--- 231221/특정 위치의 숫자 2/numbers-in-specific-location-2.py
+++ 231221/특정 위치의 숫자 2/numbers-in-specific-location-2.py
@@ -2,8 +2,13 @@ n = int(input())
 lst = list(map(int, input().split()))
 
 s = 0
+avg = 0
+
 for i in range(len(lst)):
     if i % 2 != 0:
         s += lst[i]
+        avg += 1
 
-print(s, s / (n // 2))
+
+
+print(s, '{:.1f}'.format(s / avg))
