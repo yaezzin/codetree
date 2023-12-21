7dd6dfb3e9a497c039287facebcba7c17bec9790
Yejin Jeon <97823928+yaezzin@users.noreply.github.com>
2023-12-21 18:29:07 +0900

    Title: 이동경로상에 있는 모든 숫자 더하기, Time: 133ms, Memory: 26MB, Status: Passed - CodeTree

--- 231221/이동경로상에 있는 모든 숫자 더하기/add-all-the-numbers-on-the-path.py
+++ 231221/이동경로상에 있는 모든 숫자 더하기/add-all-the-numbers-on-the-path.py
@@ -1,24 +1,14 @@
 n, t = map(int, input().split())
 command = list(input())
-lst = [[0] * n for _ in range(n)]
+lst = [ list(map(int, input().split())) for _ in range(n)]
 
 def in_range(x, y):
     return x >= 0 and x < n and y >= 0 and y < n
 
-def fill_lst(n):
-    k = 1
-    for i in range(n):
-        for j in range(n):
-            lst[i][j] = k
-            k += 1
-    
 def solution():
-    fill_lst(n)
-
     dx = [0, 1, 0, -1]
     dy = [1, 0, -1, 0]
 
-    
     cur_dir = 3 # 현재 북쪽 
     x, y = n // 2, n // 2
     s = lst[x][y]
@@ -41,7 +31,7 @@ def solution():
                 y = y + dy[cur_dir]
                 
                 s += lst[x][y]
-
+            
     print(s)
     
 solution()
