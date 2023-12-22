8dd14bbc84f684028f03e9a38a59f3ca90f3ae00
Yejin Jeon <97823928+yaezzin@users.noreply.github.com>
2023-12-22 15:32:12 +0900

    Title: 최고의 13위치 2, Time: 119ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 231222/최고의 13위치 2/best-place-of-13-2.py
+++ 231222/최고의 13위치 2/best-place-of-13-2.py
@@ -3,16 +3,14 @@ n = int(input())
 lst = [list(map(int, input().split())) for _ in range(n)]
 
 answer = 0
-
 for i in range(n):
     for j in range(n-2):
         for k in range(n):
             for l in range(n-2): 
-                if i == k and j == l:
+                if i == k and abs(l-j) < 2: # 두칸 이상 차이 나야함
                     continue
                 first_sum = lst[i][j] + lst[i][j+1] + lst[i][j+2]
                 seccond_sum = lst[k][l] + lst[k][l+1] + lst[k][l+2]
                 answer = max(answer, first_sum + seccond_sum)
-
 
 print(answer)
