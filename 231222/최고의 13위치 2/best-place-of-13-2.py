2d6d8c99479fe5459b9daec6b736bb084ed62a5d
Yejin Jeon <97823928+yaezzin@users.noreply.github.com>
2023-12-22 15:33:50 +0900

    Title: 최고의 13위치 2, Time: 149ms, Memory: 26MB, Status: Passed - CodeTree

--- 231222/최고의 13위치 2/best-place-of-13-2.py
+++ 231222/최고의 13위치 2/best-place-of-13-2.py
@@ -7,7 +7,7 @@ for i in range(n):
     for j in range(n-2):
         for k in range(n):
             for l in range(n-2): 
-                if i == k and abs(l-j) < 2: # 두칸 이상 차이 나야함
+                if i == k and abs(l-j) <= 2: # 두칸 이상 차이 나야함
                     continue
                 first_sum = lst[i][j] + lst[i][j+1] + lst[i][j+2]
                 seccond_sum = lst[k][l] + lst[k][l+1] + lst[k][l+2]
