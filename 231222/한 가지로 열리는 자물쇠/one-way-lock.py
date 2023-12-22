382286a840ed85d851fe3a8a50bf79be410f823c
Yejin Jeon <97823928+yaezzin@users.noreply.github.com>
2023-12-22 17:54:14 +0900

    Title: 한 가지로 열리는 자물쇠, Time: 199ms, Memory: 26MB, Status: Passed - CodeTree

--- 231222/한 가지로 열리는 자물쇠/one-way-lock.py
+++ 231222/한 가지로 열리는 자물쇠/one-way-lock.py
@@ -2,14 +2,14 @@ n = int(input())
 lst = list(map(int, input().split()))
 
 # 전체 경우의 수
-total = n ** len(lst)
+total = n * n * n
 
 cnt = 0
 for i in range(1, n + 1):
     for j in range(1, n + 1):
         for k in range(1, n + 1):
-            if i > lst[0] + 2 and j > lst[1] + 2 and k > lst[2] + 2:
-                cnt += 1 
-    
+            if abs(i - lst[0]) > 2 and abs(j - lst[1]) > 2 and abs(k - lst[2]) > 2:
+                #print(i, j, k)
+                cnt += 1
 
 print(total - cnt)
