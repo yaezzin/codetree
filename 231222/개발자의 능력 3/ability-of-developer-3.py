eee2ed63cb2f17e7d1c34b4694b99eeefa319e6f
Yejin Jeon <97823928+yaezzin@users.noreply.github.com>
2023-12-22 20:57:47 +0900

    Title: 개발자의 능력 3, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231222/개발자의 능력 3/ability-of-developer-3.py
@@ -0,0 +1,15 @@
+import sys
+
+lst = list(map(int, input().split()))
+total = sum(lst)
+
+min_value = sys.maxsize
+for i in range(len(lst)):
+    for j in range(len(lst)):
+        for k in range(len(lst)):
+            s1 = lst[i] + lst[j] + lst[k]
+            s2 = total - s1
+
+            min_value = min(min_value, abs(s1-s2))
+
+print(min_value)
