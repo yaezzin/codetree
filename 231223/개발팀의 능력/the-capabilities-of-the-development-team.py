42a99b34bb0e93e0d326431e5a10fe9d482233e2
Yejin Jeon <97823928+yaezzin@users.noreply.github.com>
2023-12-23 02:05:50 +0900

    Title: 개발팀의 능력, Time: 104ms, Memory: 24MB, Status: Passed - CodeTree

--- 231223/개발팀의 능력/the-capabilities-of-the-development-team.py
+++ 231223/개발팀의 능력/the-capabilities-of-the-development-team.py
@@ -7,9 +7,11 @@ total = sum(lst)
 min_diff = sys.maxsize
 flag = 0
 for i in range(len(lst)):
-    for j in range(i+1, len(lst)):
-        for k in range(j+1, len(lst)):
-
+    for j in range(len(lst)):
+        for k in range(len(lst)):
+            if i == j or i == k or j == k:
+                continue
+                
             one = lst[i]
             two = lst[j] + lst[k]
             three = total - one - two
@@ -19,7 +21,7 @@ for i in range(len(lst)):
                 max_value = max(one, two, three)
                 min_value = min(one, two, three)
 
-                min_diff = min(min_diff, max_value-min_value)
+                min_diff = min(min_diff, max_value - min_value)
                 flag = 1
 
 if flag == 0:
