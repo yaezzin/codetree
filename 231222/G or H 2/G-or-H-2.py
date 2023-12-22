f047962a0c20b90c2d9c997ba71d11e7143dfb5d
Yejin Jeon <97823928+yaezzin@users.noreply.github.com>
2023-12-22 17:12:57 +0900

    Title: G or H 2, Time: 166ms, Memory: 25MB, Status: Passed - CodeTree

--- 231222/G or H 2/G-or-H-2.py
+++ 231222/G or H 2/G-or-H-2.py
@@ -1,15 +1,16 @@
 n = int(input())
 
+max_location = 0
 lst = [0] * 101
 for _ in range(n):
     location, alphabet = input().split()
     location = int(location)
     lst[location] = alphabet
-
+    max_location = max(max_location, location)
 
 max_value = 0 
-for i in range(17):
-    for j in range(i, 17):
+for i in range(max_location + 1):
+    for j in range(i, max_location + 1):
         # 양끝점이 0이면 안됨
         if lst[i] !=0 and lst[j] !=0:
 
