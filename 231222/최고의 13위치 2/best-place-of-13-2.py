2f705c4db5cbfa42400198ed644588cccbd39da1
Yejin Jeon <97823928+yaezzin@users.noreply.github.com>
2023-12-22 15:23:23 +0900

    Title: 최고의 13위치 2, Time: 118ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231222/최고의 13위치 2/best-place-of-13-2.py
+++ 231222/최고의 13위치 2/best-place-of-13-2.py
@@ -3,12 +3,19 @@ n = int(input())
 lst = [list(map(int, input().split())) for _ in range(n)]
 
 answer = 0
-for i in range(n):
-    for j in range(n-2):
-        for k in range(i+1, n):
-            for l in range(n-2):
-                first_sum = lst[i][j] + lst[i][j+1] + lst[i][j+2]
-                seccond_sum = lst[k][l] + lst[k][l+1] + lst[k][l+2]
-                answer = max(answer, first_sum + seccond_sum)
-
+if n < 6:
+    for i in range(n):
+        for j in range(n-2):
+            for k in range(i+1, n):
+                for l in range(n-2):
+                    first_sum = lst[i][j] + lst[i][j+1] + lst[i][j+2]
+                    seccond_sum = lst[k][l] + lst[k][l+1] + lst[k][l+2]
+                    answer = max(answer, first_sum + seccond_sum)
+else:
+    for i in range(n):
+        for j in range(n-5):
+            first_sum = lst[i][j] + lst[i][j+1] + lst[i][j+2]
+            second_sum = lst[i][j+3] + lst[i][j+4] + lst[i][j+5]
+            answer = max(answer, first_sum + second_sum)
+            
 print(answer)
