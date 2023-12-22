6e23189fc88dc3988bb8132ce91c1ba4ec247670
Yejin Jeon <97823928+yaezzin@users.noreply.github.com>
2023-12-22 16:41:12 +0900

    Title: 바구니 안의 사탕 2, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231222/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 231222/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -6,7 +6,7 @@ for _ in range(n):
     lst[location] += cnt # 같은 위치에 여러 바구니가 놓일 수 있어서 더해주기
 
 max_value = 0
-for c in range(k, len(lst)-k):
+for c in range(k, len(lst)): 
     cnt = sum(lst[c-k:c+k+1])
     max_value = max(cnt, max_value)
 
