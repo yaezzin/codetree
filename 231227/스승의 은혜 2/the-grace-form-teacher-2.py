1b509dd6bb0f41c0d31fa989f72f68bbd90d5128
Yejin Jeon <97823928+yaezzin@users.noreply.github.com>
2023-12-27 17:17:26 +0900

    Title: 스승의 은혜 2, Time: 134ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231227/스승의 은혜 2/the-grace-form-teacher-2.py
+++ 231227/스승의 은혜 2/the-grace-form-teacher-2.py
@@ -4,23 +4,19 @@ p = []
 for _ in range(n):
     p.append(int(input()))
 
+p.sort()
+
 max_student = 0
 for i in range(n):
     prefix_sum, cnt = 0, 0
+    p[i] //= 2
 
     for j in range(n):
-        if i == j:
-            p[i] //= 2
-        
-        prefix_sum += p[j]
-
         if prefix_sum <= budget:
+            prefix_sum += p[j]
             cnt += 1
-        
-    
+  
     p[i] *= 2
-
-          
-    max_student = max(max_student, cnt)
+    max_student = max(max_student, cnt-1)
 
 print(max_student)
