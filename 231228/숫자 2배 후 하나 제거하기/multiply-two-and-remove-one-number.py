2fff6dc45e412575ee6a7fe89acdf812b112e817
Yejin Jeon <97823928+yaezzin@users.noreply.github.com>
2023-12-28 17:11:30 +0900

    Title: 숫자 2배 후 하나 제거하기, Time: 195ms, Memory: 26MB, Status: Wrong Answer - CodeTree

---
+++ 231228/숫자 2배 후 하나 제거하기/multiply-two-and-remove-one-number.py
@@ -0,0 +1,30 @@
+import sys
+
+n = int(input())
+lst = list(map(int, input().split()))
+
+min_diff = sys.maxsize
+for i in range(n):
+    lst[i] *= 2
+
+    tmp = []
+    
+    for j in range(n):
+        for k in range(n):
+            if j != k:
+                # 하나 제외하고 리스트에 넣기
+                tmp.append(lst[k])
+            
+        # 차이구하기
+        diff = 0
+        for l in range(len(tmp)-1):
+            diff += abs(tmp[l] - tmp[l+1])
+        
+        # 최소값 찾기
+        min_diff = min(diff, min_diff)
+
+
+    # 되돌리기
+    lst[i] //= 2
+
+print(min_diff)
