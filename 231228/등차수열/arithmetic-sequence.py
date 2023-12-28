707f9f663c8206a81223f614e43080ca4e0eaf4b
Yejin Jeon <97823928+yaezzin@users.noreply.github.com>
2023-12-28 16:31:54 +0900

    Title: 등차수열, Time: 136ms, Memory: 25MB, Status: Passed - CodeTree

--- 231228/등차수열/arithmetic-sequence.py
+++ 231228/등차수열/arithmetic-sequence.py
@@ -1,11 +1,14 @@
 n = int(input())
 a = list(map(int, input().split()))
 
-cnt = 0
-for i in range(n):
-    for j in range(i+1, n):
-        for k in range(100):
-            if abs(a[i] - k) == abs(k - a[j]):
+max_cnt = 0
+for k in range(1, 101):
+    cnt = 0
+    for i in range(n):
+        for j in range(i+1, n):
+            if abs(a[j] - k) == abs(k - a[i]):
                 cnt += 1
+                
+    max_cnt = max(max_cnt, cnt)
 
-print(cnt)
+print(max_cnt)
