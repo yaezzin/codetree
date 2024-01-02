129b497240fa6bc42541304a10293e4eccd626b9
Yejin Jeon <97823928+yaezzin@users.noreply.github.com>
2024-01-02 17:34:26 +0900

    Title: A, B, C, D 찾기 2, Time: 976ms, Memory: 27MB, Status: Passed - CodeTree

--- 240102/A, B, C, D 찾기 2/find-a-b-c-d-2.py
+++ 240102/A, B, C, D 찾기 2/find-a-b-c-d-2.py
@@ -1,12 +1,26 @@
-def is_equal(a, b, c, d, lst):
-    tmp = [a, b, c, d,
-            a + b, b + c, c + d, d + a, a + c, b + d, 
-            a + b + c, a + b + d, a + c + d, b + c + d, a + b + c + d] 
+from itertools import combinations
 
-    if all(value in lst for value in tmp):
+def is_equal(a, b, c, d, sum_lst):
+    #tmp = [ a, b, c, d,
+    #        a + b, b + c, c + d, d + a, a + c, b + d, 
+    #        a + b + c, a + b + d, a + c + d, b + c + d, a + b + c + d] 
+
+    comb = []
+    for r in range(1, len(sum_lst) + 1):
+        comb.extend(combinations([a, b, c, d], r))
+    
+    s = []
+    for c in comb:
+        s.append(sum(c))
+    
+    sum_lst.sort()
+    s.sort()
+
+    if sum_lst == s:
         return True
     
     return False
+
 
 lst = list(map(int, input().split()))
 for a in range(1, 41):
@@ -15,4 +29,4 @@ for a in range(1, 41):
             for d in range(c, 41):
                 if is_equal(a, b, c, d, lst):
                     print(a, b, c, d)
-                    exit()
+                    #exit()
