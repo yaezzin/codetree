bb0b85f0eca95093d672ace4269ae654a242475d
Yejin Jeon <97823928+yaezzin@users.noreply.github.com>
2024-01-02 18:34:26 +0900

    Title: 이상한 폭탄 3, Time: 136ms, Memory: 32MB, Status: Passed - CodeTree

--- 240102/이상한 폭탄 3/strange-bomb-3.py
+++ 240102/이상한 폭탄 3/strange-bomb-3.py
@@ -1,18 +1,17 @@
+MAX = 1000000
+
 n, k = map(int, input().split())
 lst = [int(input()) for i in range(n)]
 
-bomb = [0] * 1000001
-
+bomb = [0] * (MAX + 1)
 for i in range(n):
-    max_cnt = 0
     for j in range(i+1, n):
         if lst[i] == lst[j] and j - i <= k:
             bomb[lst[i]] += 1
 
 max_cnt = max(bomb)
-
 answer = 0
-for i in range(len(bomb)):
+for i in range(len(bomb)-1, -1, -1): # 거꾸로 도는 이유는 인덱스가 큰걸로 정답을 내야되기 때문에
     if bomb[i] == max_cnt and bomb[i] != 0:
         answer = i
         break
