03461883b5d02ac6fa35067e6ac2e0070eaddb5f
Yejin Jeon <97823928+yaezzin@users.noreply.github.com>
2024-01-03 16:30:12 +0900

    Title: 구역 청소, Time: 143ms, Memory: 24MB, Status: Passed - CodeTree

--- 240103/구역 청소/district-cleaning.py
+++ 240103/구역 청소/district-cleaning.py
@@ -1,12 +1,16 @@
-lst = [0] * 101
-
 a, b = map(int, input().split())
 c, d = map(int, input().split())
 
-for i in range(a, b):
-    lst[i] = 1
+def is_intersecting(x1, x2, x3, x4):
+    if x2 < x3 or x4 < x1: # 겹치지 않는 경우
+        return False 
+    else:
+        return True
 
-for i in range(c, d):
-    lst[i] = 1
 
-print(lst.count(1))
+# 겹친다면 제일 큰놈 - 제일 작은 놈하면 전체 구역의 크기가 나옴
+if is_intersecting(a, b, c, d):
+    print(max(b, d) - min(a, c))
+# 겹치지 않는다면 구역 2개 각각 더하기 
+else:
+    print(b-a + d-c)
