682f7e8efa97d2dad5b8d4759f0797682efe007b
Yejin Jeon <97823928+yaezzin@users.noreply.github.com>
2024-01-03 17:07:25 +0900

    Title: 비둘기와 전기줄, Time: 117ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240103/비둘기와 전기줄/pigeons-and-electric-cords.py
+++ 240103/비둘기와 전기줄/pigeons-and-electric-cords.py
@@ -1,13 +1,16 @@
+MAX_BIRD = 10
+
 n = int(input())
-location = [[0] * 2 for _ in range(11)]
+location = [[0] * 2 for _ in range(MAX_BIRD + 1)]
 
 for _ in range(n):
     num, loc = map(int, input().split())
     location[num][loc] += 1
 
+
 cnt = 0
 for l in location:
-    if l[0] != 0 and l[1] != 0:
+    if l[0] != 0 or l[1] != 0:
         cnt += min(l[0], l[1])
 
 print(cnt)
