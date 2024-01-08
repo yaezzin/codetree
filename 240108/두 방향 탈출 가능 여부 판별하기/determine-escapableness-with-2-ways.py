97673c46edfe9922243e54e19e18141f52c976b7
Yejin Jeon <97823928+yaezzin@users.noreply.github.com>
2024-01-08 20:10:01 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240108/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
+++ 240108/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -1,30 +1,47 @@
 def in_range(x, y):
-    return x >= 0 and x < n  and y >= 0 and y < n
+    return x >= 0 and x < n  and y >= 0 and y < m
+
+def can_go(x, y):
+    if not in_range(x, y):
+        return False
+        
+    if visited[x][y] == 1:
+        return False
+    
+    if adj[x][y] == 0:
+        return False
+    
+    return True 
 
 def dfs(x, y):
-    # 방문처리
+    # 방문처리 및 더해주기!
+    global order
     visited[x][y] = 1
-    adj[x][y] += 1 
+    order += 1
+    adj[x][y] = order
 
     # 아래, 오른쪽 두방향만 이동 가능
     dxs, dys = [1, 0], [0, 1]
 
+    # 방향들을 돌면서 방문 가능한지 확인
     for dx, dy in zip(dxs, dys):
         new_x, new_y = x + dx, y + dy
 
-        # 방문을 안했고, 범위 안에 있고, 뱀이 없다면 -> dfs 호출하기
-        if in_range(new_x, new_y) and not visited[new_x][new_y] and adj[new_x][new_y] != 0:
+        # 방문 가능하면 dfs 호출하기
+        if can_go(new_x, new_y):
             dfs(new_x, new_y)
 
 n, m = map(int, input().split())
-visited = [[0] * (n + 1) for _ in range(n+1)]
+visited = [[0] * (n + 1) for _ in range(m + 1)]
 adj = []
+order = 1
 for _ in range(n):
     adj.append(list(map(int, input().split())))
 
+
 dfs(0,0)
 
-if adj[n-1][n-1] == 2:
+if adj[n-1][m-1] != 0 and adj[n-1][m-1] != 0:
     print(1)
 else:
     print(0)
