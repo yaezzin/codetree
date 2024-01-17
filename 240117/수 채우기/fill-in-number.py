ad0bea657e45eb16e61a3a5015a4ba6526376b26
Yejin Jeon <97823928+yaezzin@users.noreply.github.com>
2024-01-17 15:27:55 +0900

    Title: 수 채우기, Time: 128ms, Memory: 25MB, Status: Passed - CodeTree

--- 240117/수 채우기/fill-in-number.py
+++ 240117/수 채우기/fill-in-number.py
@@ -3,7 +3,7 @@ n = int(input())
 # 5로 나눌 수 있을 때까지 2를 뺴기
 
 answer = 0
-while n != 0:
+while n > 0:
     if n % 5 != 0:
         n -= 2
 
@@ -12,4 +12,7 @@ while n != 0:
 
     answer += 1
 
-print(answer)
+if n < 0:
+    print(-1)
+else:
+    print(answer)
