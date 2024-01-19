a9ec471119a20f80685f4a7821f5624f8f36578b
Yejin Jeon <97823928+yaezzin@users.noreply.github.com>
2024-01-20 03:25:50 +0900

    Title: 사각형 채우기, Time: 154ms, Memory: 24MB, Status: Passed - CodeTree

--- 240120/사각형 채우기/rectangle-fill.py
+++ 240120/사각형 채우기/rectangle-fill.py
@@ -1,16 +1,9 @@
-def func(n):
-    if dp[n] != -1:
-        return dp[n]
-
-    if n < 2:
-        dp[n] = 1
-
-    else:
-        dp[n] = func(n-1) + func(n-2)
-    
-    return dp[n]
-
-
 n = int(input())
-dp = [-1] * (n + 1)
-print(func(n) % 10007)
+
+dp = [0] * (n + 1)
+dp[0], dp[1] = 1, 1
+
+for i in range(2, n + 1):
+    dp[i] = dp[i-1] + dp[i-2]
+
+print(dp[n] % 10007)
