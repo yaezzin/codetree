941d57e06e78a77ca78f523414cc05c28934f643
Yejin Jeon <97823928+yaezzin@users.noreply.github.com>
2024-01-20 03:27:51 +0900

    Title: 계단 오르기, Time: 132ms, Memory: 24MB, Status: Passed - CodeTree

--- 240120/계단 오르기/climbing-stairs.py
+++ 240120/계단 오르기/climbing-stairs.py
@@ -1,18 +1,13 @@
+MAX = 1000
 MOD = 10007
-def func(n):
-
-    dp = [0] * (n + 1)
-    dp[0] = 1
-    for i in range(1, n + 1):
-        if i >= 2:
-            dp[i] = dp[i] + dp[i-2] 
-       
-        if i >= 3:
-            dp[i] = dp[i] + dp[i-3] 
-    
-
-    return dp[n]
-
 
 n = int(input())
-print(func(n) % MOD)
+
+dp = [0] * (MAX + 1)
+dp[0], dp[2], dp[3] = 1, 1, 1
+dp[1] = 0
+
+for i in range(4, n + 1):
+    dp[i] = dp[i-3] + dp[i-2] 
+ 
+print(dp[n] % MOD)
