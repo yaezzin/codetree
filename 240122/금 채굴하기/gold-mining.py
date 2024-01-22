ac0323d5547d88a63542abab4eb540b0d58166d3
Yejin Jeon <97823928+yaezzin@users.noreply.github.com>
2024-01-22 19:47:00 +0900

    Title: 금 채굴하기, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240122/금 채굴하기/gold-mining.py
+++ 240122/금 채굴하기/gold-mining.py
@@ -6,37 +6,22 @@ def in_range(x, y):
     return x >= 0 and y >= 0 and x < n and y < n
 
 def find_coins(x, y, k):
-    dxs, dys = [0, 1, 1, 1, 0, -1, -1, -1], [1, 1, 0, -1, -1, -1, 0, 1]
+    s = 0
+    for i in range(n):
+        for j in range(n):
+            if in_range(x, y):
+                if i >= x-k and i <= x + k and j >= y - k and j <= y + k:
+                    s += grid[i][j]
 
-    s = grid[x][y]
-
-    for i in range(8):
-        dx, dy = dxs[i], dys[i]
-        
-        # 대각선 방향 이라면 k-1칸수만큼 확인
-        if i % 2 != 0:
-            for j in range(k-1):
-                new_x, new_y = x + (dx * (j + 1)) , y + (dy * (j + 1))
-
-                if in_range(new_x, new_y):                    
-                    s += grid[new_x][new_y]
-        
-        #위아래좌우방향이라면 K 만큼 개수 확인
-        elif i % 2 == 0:
-            for j in range(k):
-                new_x, new_y = x + (dx * (j + 1)), y + (dy * (j + 1))
-
-                if in_range(new_x, new_y):
-                    s += grid[new_x][new_y]
-    
     return s
 
 # Answer
 answer = 0
-for k in range(0, n // 2 + 1):
+for k in range(n):
     for i in range(n):
         for j in range(n):
             coin_cnt = find_coins(i, j, k)
+            #print(i, j, k, coin_cnt)
             result = coin_cnt * m - (k * k + (k + 1) * (k + 1))
             if result > 0:
                 answer = max(answer, coin_cnt)
