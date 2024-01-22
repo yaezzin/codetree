32ddcf497fecfea4223091fc8a3900701abf10c0
Yejin Jeon <97823928+yaezzin@users.noreply.github.com>
2024-01-22 23:57:29 +0900

    Title: 행복한 수열의 개수, Time: 119ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240122/행복한 수열의 개수/number-of-happy-sequence.py
@@ -0,0 +1,43 @@
+def find_happy(n, m):
+    total = 0
+    
+    # row 확인하기
+    for i in range(n):
+        row_count = 1
+        for j in range(1, n):
+            if grid[i][j-1] == grid[i][j]:
+                row_count += 1
+            
+            else:
+                row_count = 1
+
+        if row_count >= m:
+            total += 1
+    
+    # col 확인하기
+    for i in range(n):
+        col_count = 1
+        for j in range(1, n):
+            if grid[j-1][i] == grid[j][i]:
+                col_count += 1
+            
+            else:
+                col_count = 1
+        
+        if col_count >= m:
+            total += 1
+
+
+    
+            
+
+    return total
+
+n, m = map(int, input().split())
+
+grid = []
+for _ in range(n):
+    grid.append(list(map(int, input().split())))
+
+result = find_happy(n, m)
+print(result)
