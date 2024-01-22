d0b24be6fadcf2681f56c93fc2f7b45d2acbbf25
Yejin Jeon <97823928+yaezzin@users.noreply.github.com>
2024-01-22 20:59:01 +0900

    Title: 금 채굴하기, Time: 130ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240122/금 채굴하기/gold-mining.py
+++ 240122/금 채굴하기/gold-mining.py
@@ -6,12 +6,12 @@ def in_range(x, y):
     return x >= 0 and y >= 0 and x < n and y < n
 
 def find_coins(x, y, k):
-    s = 0 # 1 0 / 0 0 0 1 2 0
+    s = 0
+
     for i in range(n):
         for j in range(n):
-            if in_range(x, y):
-                if i >= x - k and i <= x + k and j >= y - k and j <= y + k:
-                    s += grid[i][j]
+            if abs(x-i) + abs(y-j) <= k:
+                s += grid[i][j]
 
     return s
 
@@ -22,6 +22,7 @@ for k in range(n):
         for j in range(n):
             coin_cnt = find_coins(i, j, k)            
             result = coin_cnt * m - (k * k + (k + 1) * (k + 1))
+            
             if result >= 0:
                 answer = max(answer, coin_cnt)
 
