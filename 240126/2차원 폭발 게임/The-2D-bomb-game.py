09024d8fbc4075cc61e10ad14aa13d453b20db03
Yejin Jeon <97823928+yaezzin@users.noreply.github.com>
2024-01-26 18:51:50 +0900

    Title: 2차원 폭발 게임, Time: 136ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240126/2차원 폭발 게임/The-2D-bomb-game.py
+++ 240126/2차원 폭발 게임/The-2D-bomb-game.py
@@ -1,51 +1,53 @@
-import copy 
+import copy
 
 def can_bomb(consec, m):
-    return any(count >= m- 1 for count in consec)
+    return any(count >= m - 1 for count in consec)
 
 def get_consec(grid, col):
     n = len(grid)
 
     cnt = 0
     consec = [0] * n
-    for i in range(n-1, 0, -1):
-        if grid[i][col] == grid[i-1][col]:
+    for i in range(n - 1, 0, -1):
+        if grid[i][col] == grid[i - 1][col]:
             cnt += 1
         else:
-            cnt = 0    
+            cnt = 0
         consec[i] = cnt
 
     return consec
 
-def bomb(grid, col, m, consective_lst):
-    for idx, cnt in enumerate(consective_lst):
+def bomb(grid, col, m, consecutive_lst):
+    for idx, cnt in enumerate(consecutive_lst):
         if cnt >= m - 1:
-            for i in range(idx-1, idx + cnt):
-                grid[i][col] = 0 
-    
+            for i in range(idx - 1, idx + cnt):
+                grid[i][col] = 0
+
 def drop(grid):
     n = len(grid)
 
     for col in range(n):
         tmp = [0] * n
         tmp_idx = n - 1
-        for row in range(n-1, -1, -1):
+        for row in range(n - 1, -1, -1):
             if grid[row][col] != 0:
                 tmp[tmp_idx] = grid[row][col]
                 tmp_idx -= 1
-        
-        for row in range(n-1, -1, -1):
-            grid[row][col] = tmp[row]
 
+        for row in range(n - 1, -1, -1):
+            grid[row][col] = tmp[row]
 
 def shift(grid):
     n = len(grid)
-    tmp = copy.deepcopy(grid)
+    rotated = [[0] * n for _ in range(n)]
 
-    # 90도 시계방향 회전
-    for col in range(n):
-        for row in range(n):
-            grid[row][col] = tmp[n-col-1][row]
+    for i in range(n):
+        for j in range(n):
+            rotated[j][n - 1 - i] = grid[i][j]
+
+    for i in range(n):
+        for j in range(n):
+            grid[i][j] = rotated[i][j]
 
 def check_bomb_cnt(grid):
     n = len(grid)
@@ -62,19 +64,26 @@ def solution():
     grid = [list(map(int, input().split())) for _ in range(n)]
 
     # k번 반복하며 m개 이상인 것들은 폭발시킨다.
-    for _ in range(1001):
+    for i in range(1001):
+        flag = 0
+
         for i in range(n):
             consecutive_lst = get_consec(grid, i)
             if can_bomb(consecutive_lst, m):
                 bomb(grid, i, m, consecutive_lst)
-        
+                flag = 1
+
         drop(grid)
         shift(grid)
         drop(grid)
+        
+        if flag == 0:
+            print('??')
+            break
 
-
+    
+        
     result = check_bomb_cnt(grid)
     print(result)
-
 
 solution()
