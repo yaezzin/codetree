e2037120d3ebef0816f977f31879b1e541959000
Yejin Jeon <97823928+yaezzin@users.noreply.github.com>
2024-01-30 17:09:48 +0900

    Title: 벽 짚고 미로 탈출하기, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240130/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.py
+++ 240130/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.py
@@ -1,6 +1,7 @@
 n = int(input())
 x, y = map(int, input().split())
 grid = [list(input()) for _ in range(n)]
+visited = [[0] * n for _ in range(n)]
 dx, dy = [0, 1, 0, -1], [1, 0, -1, 0]
 
 cur_dir = 0
@@ -9,29 +10,14 @@ answer = 0
 def in_range(x, y):
     return x >= 0 and y >= 0 and x < n and y < n
 
-def can_not_go(r, c):
-    for x, y in zip(dx, dy):
-        nx, ny = r + x, c + y
-        if in_range(nx, ny) and grid[nx][ny] == '#':
-            continue
-        else:
-            return False
-    
-    return True
-
 def simulate(x, y): 
     global cur_dir
     global answer
     
     while True:
-
-        if can_not_go(x, y):
-            answer = -1
-            break
-
         nx, ny = x + dx[cur_dir], y + dy[cur_dir]
 
-        if in_range(nx, ny):
+        if in_range(nx, ny) and not visited[nx][ny]:
             # 1 ) 바라보고 있는 방향으로 이동 불가능한 경우 반시계 방향 회전
             if grid[nx][ny] == '#': 
                 cur_dir = (cur_dir -1 + 4) % 4
@@ -45,22 +31,31 @@ def simulate(x, y):
                 # 오른쪽에 벽이 있다면 한칸 이동
                 if in_range(tmp_x, tmp_y) and grid[tmp_x][tmp_y] == '#':
                     x, y = nx, ny
+                    visited[x][y] = 1
                     answer += 1
             
                 # 없다면 한칸 이동 후 시계방향 회전하여 한칸 더 이동
                 else:
                     x, y = nx, ny
+                    visited[x][y] = 1
+
                     cur_dir = (cur_dir + 1) % 4
                     x, y = x + dx[cur_dir], y + dy[cur_dir]
+                    visited[x][y] = 1
+
                     answer += 2
-                    
-        
+
+        # 이동 가능하나 사이클이 존재하면 -1  
+        elif in_range(nx, ny) and visited[nx][ny]:
+            answer = -1
+            break
+
         # 2 ) 바라보는 방향으로 이동이 불가능하면 탈출
         else:
             answer += 1
             break
 
     print(answer)
-        
+       
 
 simulate(x-1, y-1)
