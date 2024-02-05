f9255874660b27573fd6abfa8541ebe8faa64b39
Yejin Jeon <97823928+yaezzin@users.noreply.github.com>
2024-02-05 15:47:13 +0900

    Title: 사각형 채우기 3, Time: 151ms, Memory: 73MB, Status: Passed - CodeTree

---
+++ 240205/사각형 채우기 3/rectangle-fill-3.py
@@ -0,0 +1,17 @@
+MAX_N = 1000
+MOD = 1000000007
+
+n = int(input())
+
+dp = [0] * (MAX_N + 1)
+
+dp[0] = 1
+dp[1] = 2
+
+for i in range(2, n + 1):
+    dp[i] = (2 * dp[i-1] + 3 * dp[i-2]) % MOD
+
+    for j in range(i-3, -1, -1):
+        dp[i] = (dp[i] + dp[j] * 2) % MOD
+
+print(dp[n])
