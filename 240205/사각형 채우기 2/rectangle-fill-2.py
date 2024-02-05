15e2241b9eae2a775e408b75f30ae24dc94f0a2b
Yejin Jeon <97823928+yaezzin@users.noreply.github.com>
2024-02-05 16:30:30 +0900

    Title: 사각형 채우기 2, Time: 144ms, Memory: 24MB, Status: Passed - CodeTree

--- 240205/사각형 채우기 2/rectangle-fill-2.py
+++ 240205/사각형 채우기 2/rectangle-fill-2.py
@@ -1,7 +1,8 @@
 MAX_N = 1000
+MOD = 10007
 
 n = int(input())
-dp = [0] * MAX_N
+dp = [0] * (MAX_N + 1)
 
 dp[0] = 1
 dp[1] = 1
@@ -9,4 +10,4 @@ dp[1] = 1
 for i in range(2, n + 1):
     dp[i] = dp[i-1] + 2 * dp[i-2]
 
-print(dp[n])
+print(dp[n] % MOD)
