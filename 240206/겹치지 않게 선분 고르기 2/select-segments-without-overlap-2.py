9c5f432e92aeafbff489e69a37c73cca39bee625
Yejin Jeon <97823928+yaezzin@users.noreply.github.com>
2024-02-06 17:50:55 +0900

    Title: 겹치지 않게 선분 고르기 2, Time: 127ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240206/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.py
@@ -0,0 +1,16 @@
+n = int(input())
+times = []
+
+for _ in range(n):
+    times.append(list(map(int, input().split())))
+
+times.sort(key=lambda x : x[1])
+
+cnt = 1
+pivot = times[0]
+for i in range(1, n):
+    if pivot[1] <= times[i][0]:
+        pivot = times[i]
+        cnt += 1
+    
+print(cnt)
