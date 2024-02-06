7dcc37657bb34d90243167b22d5712de230a1738
Yejin Jeon <97823928+yaezzin@users.noreply.github.com>
2024-02-06 17:45:34 +0900

    Title: 2차원 최대 증가 수열, Time: 237ms, Memory: 26MB, Status: Passed - CodeTree

--- 240206/2차원 최대 증가 수열/longest-increasing-sequence-2d.py
+++ 240206/2차원 최대 증가 수열/longest-increasing-sequence-2d.py
@@ -6,6 +6,9 @@ def in_range(x, y):
     return 0 <= x < n and 0 <= y < m
 
 def solution(x, y):
+    if dp[x][y] != 0:
+        return dp[x][y]
+        
     max_cnt = 1
     for i in range(x+1, n):
         for j in range(y+1, m):
