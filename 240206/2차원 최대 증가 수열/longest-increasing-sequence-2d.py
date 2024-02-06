6e5b82aa38d730c4213079cb843ea18aba522d5a
Yejin Jeon <97823928+yaezzin@users.noreply.github.com>
2024-02-06 17:44:34 +0900

    Title: 2차원 최대 증가 수열, Time: 5039ms, Memory: 28MB, Status: Time Limit Exceed - CodeTree

--- 240206/2차원 최대 증가 수열/longest-increasing-sequence-2d.py
+++ 240206/2차원 최대 증가 수열/longest-increasing-sequence-2d.py
@@ -3,7 +3,7 @@ grid = [list(map(int, input().split())) for _ in range(n)]
 dp = [[0] * m for _ in range(n)]
 
 def in_range(x, y):
-    return 0 <= x < n and 0 <= y < n
+    return 0 <= x < n and 0 <= y < m
 
 def solution(x, y):
     max_cnt = 1
@@ -13,8 +13,8 @@ def solution(x, y):
                 max_cnt = max(max_cnt, solution(i, j) + 1)
     
     dp[x][y] = max_cnt
+    
     return max_cnt
 
 solution(0, 0)
-
 print(dp[0][0])
