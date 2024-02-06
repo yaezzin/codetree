3a9ff936020eb78a15c595daf58d121f60d10bd5
Yejin Jeon <97823928+yaezzin@users.noreply.github.com>
2024-02-06 17:52:47 +0900

    Title: 겹치지 않게 선분 고르기 2, Time: 169ms, Memory: 25MB, Status: Passed - CodeTree

--- 240206/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.py
+++ 240206/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.py
@@ -9,7 +9,7 @@ times.sort(key=lambda x : x[1])
 cnt = 1
 pivot = times[0]
 for i in range(1, n):
-    if pivot[1] <= times[i][0]:
+    if pivot[1] < times[i][0]:
         pivot = times[i]
         cnt += 1
     
