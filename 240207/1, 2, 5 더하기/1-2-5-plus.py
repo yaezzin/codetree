10126fa90212d5882c3e3352d10c0954a7eff636
Yejin Jeon <97823928+yaezzin@users.noreply.github.com>
2024-02-07 16:09:36 +0900

    Title: 1, 2, 5 더하기, Time: 145ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240207/1, 2, 5 더하기/1-2-5-plus.py
+++ 240207/1, 2, 5 더하기/1-2-5-plus.py
@@ -3,7 +3,6 @@ import sys
 INT_MAX = sys.maxsize
 MOD = 10007
 
-
 n = int(input())
 nums = [1, 2, 5]
 dp = [0] * (n + 1)
@@ -14,7 +13,7 @@ def solution():
     for i in range(1, n + 1):
         for j in range(len(nums)):
             if i >= nums[j]:
-                dp[i] = (dp[i] + dp[i - nums[j]]) % MOD
+                dp[i] += dp[i - nums[j]] % MOD
 
 solution()
 print(dp[n])
