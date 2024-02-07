8e8b71559f9421e173816cafdba24ede5051b58e
Yejin Jeon <97823928+yaezzin@users.noreply.github.com>
2024-02-07 16:07:00 +0900

    Title: 1, 2, 5 더하기, Time: 152ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240207/1, 2, 5 더하기/1-2-5-plus.py
@@ -0,0 +1,20 @@
+import sys
+
+INT_MAX = sys.maxsize
+MOD = 10007
+
+
+n = int(input())
+nums = [1, 2, 5]
+dp = [0] * (n + 1)
+
+def solution():
+    dp[0] = 1
+
+    for i in range(1, n + 1):
+        for j in range(len(nums)):
+            if i >= nums[j]:
+                dp[i] = (dp[i] + dp[i - nums[j]]) % MOD
+
+solution()
+print(dp[n])
