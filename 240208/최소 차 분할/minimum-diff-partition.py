e804ce05d514da56592887c70b49de51eea46b73
Yejin Jeon <97823928+yaezzin@users.noreply.github.com>
2024-02-08 16:56:17 +0900

    Title: 최소 차 분할, Time: 168ms, Memory: 25MB, Status: Passed - CodeTree

--- 240208/최소 차 분할/minimum-diff-partition.py
+++ 240208/최소 차 분할/minimum-diff-partition.py
@@ -4,16 +4,16 @@ n = int(input())
 nums = list(map(int, input().split()))
 
 s = sum(nums)
-dp = [False] * (s + 1)
+dp = [False] * s
 dp[0] = True
 
 # dp[7] = 7을 합으로 만들 수 있는가?
 for i in range(len(nums)):
-    for j in range(s, -1 , -1):
-        if j >= nums[i] and not dp[j-nums[i]]:
+    for j in range(s-1, -1 , -1):
+        if j >= nums[i] and dp[j-nums[i]]:
             dp[j] = True
 
-
+#print(dp)
 answer = sys.maxsize
 for i in range(1, s):
     if dp[i]:
