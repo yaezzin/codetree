4cbc468506b18e322ae7e5aff9a2d49c5b06d772
Yejin Jeon <97823928+yaezzin@users.noreply.github.com>
2024-02-20 16:38:59 +0900

    Title: 자주 등장한 top k 숫자, Time: 333ms, Memory: 38MB, Status: Passed - CodeTree

--- 240220/자주 등장한 top k 숫자/top-k-frequent-elements.py
+++ 240220/자주 등장한 top k 숫자/top-k-frequent-elements.py
@@ -1,20 +1,11 @@
+# 자주 등장한 순서대로 k개 출력 - 횟수가 동일하면 값이 더 큰숫자 먼저 출력
 from collections import Counter
 
 n, k = map(int, input().split())
 nums = list(map(int, input().split()))
+nums.sort(reverse=True)
+counts = Counter(nums)
 
-dic = {}
-for n in nums:
-    if n in dic:
-        dic[n] += 1
-    else:
-        dic[n] = 1
-
-answer = []
-max_n = max(dic.values())
-for k, v in dic.items():
-    if v == max_n:
-        answer.append(k)
-
-answer.sort(reverse=True)
-print(*answer, end = ' ')
+result = sorted(counts.most_common(k), key=lambda x: (-x[1], -x[0]))
+for r in result:
+    print(r[0], end = ' ')
