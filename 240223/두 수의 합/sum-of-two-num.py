712343021961e52b1cfa35bf628f80d86403a57a
Yejin Jeon <97823928+yaezzin@users.noreply.github.com>
2024-02-23 16:59:55 +0900

    Title: 두 수의 합, Time: 215ms, Memory: 36MB, Status: Passed - CodeTree

--- 240223/두 수의 합/sum-of-two-num.py
+++ 240223/두 수의 합/sum-of-two-num.py
@@ -2,19 +2,20 @@ n, k = map(int, input().split())
 nums = list(map(int, input().split()))
 
 count = {}
-answer= 0
-        
-for n in nums:
-    diff = k - n 
 
-    # 딕셔너리에 diff가 있으면 가능한 모든 쌍의 수를 더해줌
+for e in nums:
+    if e in count:
+        count[e] += 1
+    else:
+        count[e] = 1
+
+answer = 0
+for i in range(n):
+    count[nums[i]] -= 1
+    
+    diff = k - nums[i]
+        
     if diff in count:
         answer += count[diff]
-    
-    # 현재 숫자의 개수를 하나 증가 시킴
-    if n in count:
-        count[n] += 1
-    else:
-        count[n] = 1
- 
+
 print(answer)
