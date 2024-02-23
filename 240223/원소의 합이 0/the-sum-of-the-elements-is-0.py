a60f3eb3ba935c1246718e9de00b7539a437143d
Yejin Jeon <97823928+yaezzin@users.noreply.github.com>
2024-02-23 17:16:32 +0900

    Title: 원소의 합이 0, Time: 365ms, Memory: 55MB, Status: Passed - CodeTree

--- 240223/원소의 합이 0/the-sum-of-the-elements-is-0.py
+++ 240223/원소의 합이 0/the-sum-of-the-elements-is-0.py
@@ -14,14 +14,16 @@ ans = 0
 for i in range(n):
     for j in range(n):
         d1[a1[i] + a2[j]] += 1
+
 for i in range(n):
     for j in range(n):
         d2[a3[i] + a4[j]] += 1
 
 answer = 0
 for k1 in d1.keys():
-    for k2 in d2.keys():
-        if k1 + k2 == 0:
-            answer += d1[k1] * d2[k2]
+    diff = 0 - k1
+
+    if diff in d2: 
+        answer += d1[k1] * d2[diff]
 
 print(answer)
