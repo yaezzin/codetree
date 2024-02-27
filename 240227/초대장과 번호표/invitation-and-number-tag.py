9a52e48bc6144b5e6097f5bf6e217acb0c0ef795
Yejin Jeon <97823928+yaezzin@users.noreply.github.com>
2024-02-27 16:39:26 +0900

    Title: 초대장과 번호표, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240227/초대장과 번호표/invitation-and-number-tag.py
+++ 240227/초대장과 번호표/invitation-and-number-tag.py
@@ -1,22 +1,28 @@
 n, g = map(int, input().split())
 
 answer = set()
+answer.add(1) # 첫번째 사람은 무조건 초대하기
+
 for _ in range(g):
     inp = list(map(int, input().split()))
     
     # 각 그룹의 인원 수 
     k = inp[0]
 
-    # 첫번째는 정답에 넣는다.
-    cur_k = 1
-    answer.add(inp[1])
+    # 나머지 인원
+    people = inp[1:]
+
+
+    cur_k = 0
+    for i in range(k):
+        # 현재 그룹 내에서 초대된 사람 수 체크
+        if people[i] in answer:
+            cur_k += 1
     
-    # 두번쨰 부터는 체크하기
-    for i in range(2, k + 1):
-        if cur_k == k - 1 and inp[i] not in answer:
-            answer.add(inp[i])
-        
-        elif len(answer) == k - 1 and inp[i] not in answer:
-            answer.add(inp[i])
+    for i in range(k):
+
+        if cur_k == k - 1 and people[i] not in answer:
+            answer.add(people[i])
+
 
 print(len(answer))
