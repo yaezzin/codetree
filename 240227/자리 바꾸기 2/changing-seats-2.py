639ab19ff8732d5c4fb6ce7e552c055f7831bb7d
Yejin Jeon <97823928+yaezzin@users.noreply.github.com>
2024-02-27 15:25:13 +0900

    Title: 자리 바꾸기 2, Time: 758ms, Memory: 65MB, Status: Passed - CodeTree

--- 240227/자리 바꾸기 2/changing-seats-2.py
+++ 240227/자리 바꾸기 2/changing-seats-2.py
@@ -8,7 +8,7 @@ for i in range(1, n + 1):
     answer[i].add(i)
 
 for i in range(3 * k):
-    shift = op[i % 4]
+    shift = op[i % k]
 
     # 자리를 서로 바꿔준다.
     nums[shift[1]], nums[shift[0]] = nums[shift[0]], nums[shift[1]]
@@ -16,7 +16,14 @@ for i in range(3 * k):
     # nums[shift[0]]은 바뀌는 수 그자체임
     # 그 바뀌는 수가 '어떤 X번째 자리(=shift[1] 그 자체)'에 갈 수 있는지를 넣어줘야함
     answer[nums[shift[0]]].add(shift[1])
+    answer[nums[shift[0]]].add(shift[0])
+    
+    answer[nums[shift[1]]].add(shift[1])
     answer[nums[shift[1]]].add(shift[0])
+    
+    #print(shift[0], shift[1])
+    #print(nums)
+    #print(answer)
 
 for i in range(1, n + 1):
     print(len(answer[i]))
